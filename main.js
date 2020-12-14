64d14691fdbe5d5edf4a6de959e3db51875131e3
salvoab <salvatore.abate85@gmail.com>
2020-12-14 14:20:49 +0100

    Mostrato messaggio in h1
    Mostrato in h1 la proprietà messaggio dell'oggetto app, istanza di classe Vue

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 /* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
    Bonus: Aggiungere un’immagine presa anch’essa da un data */
 
+let app = new Vue({
+    el: '#root',
+    data: {
+        messaggio: 'Messaggio da mostrare in h1'
+    }
+});
