f9a932b0110c461d7ef86a871e957925a1332dc2
salvoab <salvatore.abate85@gmail.com>
2020-12-14 14:55:27 +0100

    Cambiati nomi degli array
    Cambiati i nomi degli array e in html aggiunto style per la larghezza dell'immagine

--- bonus/main.js
+++ bonus/main.js
@@ -4,16 +4,16 @@
 let app = new Vue({
     el: '#root',
     data: {
-        titoloImmagine: ['Drago con donna elfo alata', 'Drago Ala blu', 'Drago Verde'],
+        titoliImmagini: ['Drago con donna elfo alata', 'Drago Ala blu', 'Drago Verde'],
         indice: 0,
-        srcImage: ['https://lh3.googleusercontent.com/proxy/-fS-ioXefF4czZj_Xf0sTzIxQ9dCU9NP2e6bFlfxPm5DaCE1QMQsM67zJZ41ArrJNJfiSYiQrWzxw9c1pdvG1TVHJ0vvo1Gb7uxi-PeR',
+        srcImages: ['https://lh3.googleusercontent.com/proxy/-fS-ioXefF4czZj_Xf0sTzIxQ9dCU9NP2e6bFlfxPm5DaCE1QMQsM67zJZ41ArrJNJfiSYiQrWzxw9c1pdvG1TVHJ0vvo1Gb7uxi-PeR',
         'https://sites.google.com/site/elmassariol/_/rsrc/1329933253897/miti-e-simboli/2--nicola-pessella--il-drago-e-il-suo-simbolismo-iconografico/Dragone.jpg',
         'https://www.friulioggi.it/wp-content/uploads/2020/04/drago-verde.jpg'
         ]
     },
     methods: {
         nextIndex: function(){
-            this.indice = (this.indice + 1) % this.srcImage.length;
+            this.indice = (this.indice + 1) % this.srcImages.length;
         }
     }
 
