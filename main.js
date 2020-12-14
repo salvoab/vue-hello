33c2031f31b688ab4bbf9a85f3ba5b4ee1ba87e5
salvoab <salvatore.abate85@gmail.com>
2020-12-14 14:25:21 +0100

    Attributo src del tag img inserito dinamicamente con Vue

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 let app = new Vue({
     el: '#root',
     data: {
-        messaggio: 'Messaggio da mostrare in h1'
+        messaggio: 'Messaggio da mostrare in h1',
+        srcImage: 'https://lh3.googleusercontent.com/proxy/-fS-ioXefF4czZj_Xf0sTzIxQ9dCU9NP2e6bFlfxPm5DaCE1QMQsM67zJZ41ArrJNJfiSYiQrWzxw9c1pdvG1TVHJ0vvo1Gb7uxi-PeR'
     }
 });
